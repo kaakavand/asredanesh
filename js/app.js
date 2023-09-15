64630277d6eadc2ae0d9c375b6cff89ee61d5147
Amirhosein Kakavand <94612720+kaakavand@users.noreply.github.com>
2023-09-15 18:22:12 +0330

    create task

--- js/app.js
+++ js/app.js
@@ -1,9 +1,30 @@
 const hamburgerMenu = document.getElementById('hamburger-menu')
+const scrollButton = document.getElementById('scroll')
+const buttonCloseMenu = document.getElementById('menu_close')
 const aside = document.querySelector('aside')
 
 hamburgerMenu.addEventListener('click' , () => {
-  console.log('amirhosein');
   aside.style.display = 'block'
   aside.style.width = '80%'
-  console.log(aside);
+  buttonCloseMenu.style.display = 'block'
 })
+
+buttonCloseMenu.addEventListener('click' , () => {
+  aside.style.display = 'none'
+  aside.style.width = '0%'
+  buttonCloseMenu.style.display = 'none'
+})
+
+scrollButton.addEventListener('click' , () => {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+})
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+}
+
+window.onscroll = function() {scrollFunction()};
